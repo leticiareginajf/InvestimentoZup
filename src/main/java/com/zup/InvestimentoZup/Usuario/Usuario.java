8c3396c5bf81f5240879a42fb298d2cbcd0aecc9
leticia.reginajf <leticia.reginajf@gmail.com>
2021-09-02 15:31:12 -0300

    Adicionando Pasta: Usuário

---
+++ src/main/java/com/zup/InvestimentoZup/Usuario/Usuario.java
@@ -0,0 +1,37 @@
+package com.zup.InvestimentoZup.Usuario;
+
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Usuario(){
+
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
