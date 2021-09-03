0ab70dac9e1ba4410892ac1dbec087bcd6abcceb
leticia.reginajf <leticia.reginajf@gmail.com>
2021-09-03 15:07:32 -0300

    Incluindo Repository e Usuario Service

---
+++ src/main/java/com/zup/InvestimentoZup/Usuario/Repository.java
@@ -0,0 +1,10 @@
+package com.zup.InvestimentoZup.Usuario;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Repository extends CrudRepository<Usuario, String> {
+
+    
+
+
+}
---
+++ src/main/java/com/zup/InvestimentoZup/Usuario/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.zup.InvestimentoZup.Usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+
+
+
+
+}
